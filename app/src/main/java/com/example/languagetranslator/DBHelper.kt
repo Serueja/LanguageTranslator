bb2a024aa82d7c175691b5a674035c7fb8be3e19
juliahgem <yulya.goryachkovskaya@gmail.com>
2023-11-01 09:59:24 +0300

    added SQL

--- app/src/main/java/com/example/languagetranslator/DBHelper.kt
+++ app/src/main/java/com/example/languagetranslator/DBHelper.kt
@@ -1,16 +1,30 @@
 package com.example.languagetranslator
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.core.content.contentValuesOf
 
 class DBHelper(context:Context): SQLiteOpenHelper(context, "History", null, 1) {
     override fun onCreate(p0: SQLiteDatabase?) {
-        p0?.execSQL("create table History(name TEXT)")
+        p0?.execSQL("create table History(source TEXT primary key, target TEXT)")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+        p0?.execSQL( "drop table if exists History")
     }
 
 }
+
+    fun savehistory(source:String, target:String): {
+        val p0 = this.writableDAtabase
+        val cv = ContentValues
+        cv.put ("source", source)
+        cv.put ("target", target)
+        val result = p0.insert("History", null, cv)
+        if (result==-1 .toLong()){
+            return false
+        }
+        return true
+    }
